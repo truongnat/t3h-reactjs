a2fde3efc01c403109e776eed746631cbec70c2b
Tran Anh Van <anhvandz02@gmail.com>
2022-07-10 01:55:57 +0700

    edit_task_face

--- Tuhoc/Fb_by_js/main.js
+++ Tuhoc/Fb_by_js/main.js
@@ -1,78 +1,69 @@
+var menuHeader = document.getElementById("menu-header");
 
-// Menu all 
-var headertop = document.getElementById("menu__header");
-headertop.classList=["container-fluid"];
+    var menuLeft = document.createElement("div");
+        menuLeft.classList=["menu-left"];
+          var menuLeftIcon = document.createElement("div");
+              menuLeftIcon.classList=["menu-left-icon"];
+                var iconFace = document.createElement("div");
+                    iconFace.classList = ["icon-face"];
+                      var faceImg = document.createElement("img");
+                          faceImg.src = "img/facebook.png";  
+                var inputLeft = document.createElement("div");
+                    inputLeft.classList=['input-left'];
+                    var iconSearch = document.createElement("img");
+                        iconSearch.src="img/search.png";
+                    var inputLeftIn = document.createElement("input");
+                        inputLeftIn.placeholder="Tìm kiếm..."
+menuHeader.appendChild(menuLeft);
+    menuLeft.appendChild(menuLeftIcon);
+        menuLeftIcon.appendChild(iconFace);
+              iconFace.appendChild(faceImg);
+         menuLeftIcon.appendChild(inputLeft);
+              inputLeft.appendChild(iconSearch);
+              inputLeft.appendChild(inputLeftIn);
 
-// menu left
-var menuLeft = document.createElement("div");
-    menuLeft.classList=["menu__Search-left"];
-var img = document.createElement("img");
-    img.classList=["icon-fb"];
-img.src = "img/facebook.png";
-var input = document.createElement("input");
-input.classList=["text-input"];
-input.tyle="Search";
-input.placeholder="Tìm kiếm trền Facebook";
 
 
-headertop.appendChild (menuLeft);
-menuLeft.appendChild (img);
-menuLeft.appendChild(input);
+    var menuCenter = document.createElement("div");
+        menuCenter.classList=["menu-center"];
+          var centerIcon = document.createElement("div");
+              centerIcon.classList= ["center-icon"];
+                var listImg = document.createElement("div");
+                     listImg.classList = ["list-img"];
+                       var sum = ["img/home.png",
+                                "img/video (1).png",
+                                "img/market.png",
+                               "img/people.png",
+                               "img/rubik.png",          
+                               ];
+                            for ( let i = 0 ; i < sum.length; i++)
+                            {
+                             let images = document.createElement("img");
+                                 images.src =sum[i];
+                                listImg.appendChild(images); 
 
-// onclick on input
+                              for(var j = 0 ; j < listImg.length ; j++)
+                              {
+                                    listImg  = sum[images];
+                              }
+                            }        
+menuHeader.appendChild(menuCenter); 
+     menuCenter.appendChild(centerIcon);
+       centerIcon.appendChild(listImg);
+      
 
 
-// var x = document.getElementsByClassName("text-input");
-//        x.addEventListener("click", function()
-//     {
-//       var y = document.getElementsByClassName("icon-fb");
-//       if(y.style.display==="none"){
-//         x.style.display="block";
-//       }else
-//       {
-//         y.style.display="none";
-//       }
-    
-//     })
+ var menuRight = document.createElement("div");
+     menuRight.classList=["menu-right"];
 
-    
-// menu Center
-var menuCenter = document.createElement("div");
-menuCenter.classList=["menu__center"];
+     var groupImgRight = document.createElement("div");
+         groupImgRight.classList=["img-Right"];
 
-var groupImage=document.createElement("div");
-groupImage.classList = ["group-img"];
- 
-var sum = ["img/home.png",
-           "img/video (1).png",
-           "img/market.png",
-           "img/people.png",
-           "img/rubik.png",          
-          ];
-
-for(let i = 0 ; i < sum.length; i++)
-{
-    let images = document.createElement("img");
-    images.src= sum[i];
-    images.classList=["image"]; 
-    groupImage.appendChild(images);
-  
-}
-menuCenter.appendChild(groupImage);
-
-// menu Right
-
-var menuRight = document.createElement("div");
-menuRight.classList=["menu__Right"];
-
-var groupImgRight = document.createElement("div");
-groupImgRight.classList=["img-Right"];
-
-var total = ["img/list.png",
-             "img/messenger.png",
-             "img/bell.png",
-             "img/van.jpg",
-            ]
+             var total = ["img/list.png",
+                          "img/messenger.png",
+                          "img/bell.png",
+                          "img/van.jpg",
+                          ];
 
  for(let j = 0 ; j < total.length; j++)
  {
@@ -81,113 +72,90 @@ var total = ["img/list.png",
     img.src = total[j];
     groupImgRight.appendChild(img);
  }       
-    
 menuRight.appendChild(groupImgRight);
-headertop.appendChild (menuCenter);
-headertop.appendChild (menuRight);
+menuHeader.appendChild(menuRight);
 
 // cover imgae
- var cover = document.getElementById("cover-image");
- cover.classList=["container-fluid cover-imge-img"];
- var coverImg = document.createElement("img");
-     coverImg.classList=["reponsive-img"];
-    coverImg.src=["img/2.jpg"];
-    coverImg.style.width = 500 + "px";
-    coverImg.style.height = 250 + "px";
-    coverImg.style.borderRadius= 5 +"px";
-   
-     
-var iconCamera = document.createElement("div");
-      iconCamera.classList=["iconCamera"];
-  var editCover = document.createElement("img");
-      editCover.src=["img/camera.png"];
-      editCover.style.width= 10+ "px";
-      editCover.style.height= 10 + "px";
-      editCover.style.display="none";
-  var editCoverText = document.createElement("button");
-      editCoverText.textContent="Chỉnh sửa ảnh bia";
- 
-iconCamera.appendChild(editCover);
-iconCamera.appendChild(editCoverText);
-cover.appendChild(iconCamera);
-cover.appendChild(coverImg);
 
-// 
-var nameMyself = document.getElementById("name-myself");
-var nameMyselfall = document.createElement("div");
-   nameMyselfall.classList=["name-myself-all"];
-     var nameMyselftText = document.createElement("div");
-         nameMyselftText.classList=["name-myself__text"];
-           var nameMyselfImg = document.createElement("div"); 
-                nameMyselfImg.classList=["name-myself__img"];
-                  var img = document.createElement("img");
-                     img.src =["img/van.jpg"];
-                  var nameText = document.createElement("div");
-                      nameText.classList=["name"];
-                         var $ = document.createElement("p");
-                            $.textContent="Trần Văn";
-                         var $$ = document.createElement("p");
-                            $$.textContent="1.6k bạn bè";
-                         var $$$ = document.createElement("p");
-                            var imgAll = ["img/van.jpg",
-                                          "img/van.jpg",
-                                          "img/van.jpg",
-                                          "img/van.jpg",
-                                          "img/van.jpg",
-                                          "img/van.jpg",          
-                                         ]
-                            for(let i = 0 ; i < imgAll.length ; i++)
-                            {
-                                let img = document.createElement("img");
-                                   img.src = imgAll[i];
-                                   $$$.appendChild(img);
-                            }
+function myFunction() {
+        var x = document.getElementById("list");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
 
-        var addEditBtn = document.createElement("div");
-             addEditBtn.classList=["add__edit-btn"];
+      // name myself
+ var nameMyself = document.getElementById("name-myself");
+    var nameMyselfAll = document.createElement("div");
+        nameMyselfAll.classList=["name-myself-all"];
+            var imgMyself = document.createElement("div");
+                 imgMyself.classList=["img-myself"];
+                    var imgAvatar = document.createElement("img");
+                       imgAvatar.src="img/van.jpg";
+            var ok = document.createElement("div");
+                ok.classList=["ok"];
+                   var imgText = document.createElement("div");
+                        imgText.classList=["img-mytext"];
+                           var text1 = document.createElement("p");
+                                text1.innerHTML = "Trần Văn";
+                           var text2 = document.createElement("p");
+                                text2.innerHTML = "1.6k bạn bè";
+                                text2.style.fontSize= 9 +"px";
+                              
+                    var imgFriends = document.createElement("div");
+                        imgFriends.classList = ["img-friends"];
+                          var tong = ["img/camera.png",
+                                     "img/van.jpg",
+                                     "img/van.jpg",
+                                     "img/van.jpg",
+                                     "img/van.jpg",
+                                     "img/van.jpg",
+                                     "img/van.jpg",
+                                     "img/van.jpg",
+                                     ]
+                                  for(let k = 0 ; k < tong.length ; k++)
+                                  {
+                                    let imgFriend = document.createElement("img");                                 
+                                     imgFriend.src =tong[k];
+                                     imgFriends.appendChild(imgFriend);
 
-               var addTin = document.createElement("div");
-                   addTin.classList= ["add_tin"];
-                   var btnAddIcon = document.createElement("img");
-                       btnAddIcon.src="img/sum.png";   
-                   var btnAddBtn = document.createElement("button");
-                       btnAddBtn.textContent = "Thêm vào tin";
+                                  }
+                     var selfEdit = document.createElement("div");
+                        selfEdit.classList =["self-edit"];
+                         var addTin = document.createElement("div");
+                             addTin.classList = ["add-tin"];
+                               var imgAddtin = document.createElement("img");
+                                   imgAddtin.src = "img/sum.png";
+                               var addBtn = document.createElement("button");                                
+                                   addBtn.innerHTML = "Thêm vào tin";
+                         var editMyself = document.createElement("div");
+                             editMyself.classList=["edit-myself"];
+                              var imgEdit = document.createElement("img");
+                                  imgEdit.src = "img/pen.png";
+                              var editBtn = document.createElement("button");                              
+                                  editBtn.innerHTML="Chỉnh sửa trang cá nhân";
+                         
 
-               var btnEdit = document.createElement("div");   
-                   btnEdit.classList = ["btn-edit"];
-                   var btnIconPen = document.createElement("img");
-                       btnIconPen.src = "img/pen.png";
-                    var btnEditButton = document.createElement("button");
-                        btnEditButton.textContent="Chỉnh sửa trang cá nhân";        
-               
-   
-   nameMyself.appendChild(nameMyselfall);
-   nameMyselfall.appendChild(nameMyselftText);
-   nameMyselftText.appendChild(nameMyselfImg);
-   nameMyselfImg.appendChild(img);
-   nameMyselfImg.appendChild(nameText);
-   nameText.appendChild($);
-   nameText.appendChild($$);
-   nameText.appendChild($$$);
-   nameMyselfall.appendChild(addEditBtn);
+        nameMyself.appendChild(nameMyselfAll);
+             nameMyselfAll.appendChild(imgMyself);
+                  imgMyself.appendChild(imgAvatar);
+             nameMyselfAll.appendChild(ok);
+                  ok.appendChild(imgText); 
+                      imgText.appendChild(text1);
+                      imgText.appendChild(text2);
+                  ok.appendChild(imgFriends);
+                  nameMyselfAll.appendChild(selfEdit);
+                     selfEdit.appendChild(addTin)
+                      addTin.appendChild(imgAddtin);
+                      addTin.appendChild(addBtn);
 
-   addEditBtn.appendChild(addTin);
-   addTin.appendChild(btnAddIcon);
-   addTin.appendChild(btnAddBtn);
+                     selfEdit.appendChild(editMyself);
+                       editMyself.appendChild(imgEdit);
+                       editMyself.appendChild(editBtn);
 
-   addEditBtn.appendChild(btnEdit);
-   btnEdit.appendChild(btnIconPen);
-   btnEdit.appendChild(btnEditButton);
-//
-// const $ = document.querySelector.bind(document);
-// const $$ = document.querySelectorAll.bind(document);
-
-// const tabs = $$('.item')
-
-// tabs.forEach((tab, index) => {
-//     tab.onclick = function()
-//     { 
-//         $('.item.actives').classList.remove('actives')
-//         this.classList.add('actives')
-//     }
-// })    
+var $ = document.getElementsByClassName("edit-cover-text");
+var $$ = document.getElementsByTagName("p"
+);
+$$.innerHTML="ok";
--- task_home/function/main.js
+++ task_home/function/main.js
@@ -1,6 +1,12 @@
-
-    function cong (a , b) {
-        return a + b ;
-    }
-    var result = cong(2,8) ;
-    console.log(result);
+// //   1
+//     function cong (a , b) {
+//         return a + b ;
+//     }
+//     var result = cong(2,8) ;
+//     console.log(result);
+var printsToBe = [];
+for (var i = 0 ; i < 3 ; i++)
+{
+    printsToBe.push(i);
+}
+printsToBe.forEach(f => console.log(f));
