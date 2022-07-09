d125128039f959e5b65d248a46a09820c38f9de5
nvtaikma <nvtai0202@gmail.com>
2022-07-09 23:26:16 +0700

    homework 7/9

--- nvtaikma/js_DOM/bai_1/main.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.createElement("IMG");
-    x.setAttribute("src", "https://icdn.dantri.com.vn/thumb_w/640/2017/1-1510967806416.jpg");
-    x.setAttribute("width", "304");
-    x.setAttribute("height", "228");
-    x.setAttribute("alt", "The Pulpit Rock");
-    document.body.appendChild(x);
-  }
