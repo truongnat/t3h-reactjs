14e3b65fff151e9727fd95ed7dc604ad7e421030
thuhuong02 <thu81358@gmail.com>
2022-07-13 23:21:19 +0700

    huong-3107

--- nguyenthithuhuong/info-huong/info-huong.js
+++ nguyenthithuhuong/info-huong/info-huong.js
@@ -1,8 +1,30 @@
-alert('ádkljvbakshd')
-var Person = {
-    name: 'Hương',
-    age: 20
-}
+// var Person = {
+//     name: 'Hương',
+//     age: 20,
+//     height: 158,
+//     run: function () {
+//         return this.age
+//     },
 
-let him = Person;
-alert(him.name)
+
+// }
+
+// var Dog = {
+//     type: 'fogi',
+//     ageWeek: 16,
+//     sound: () => {
+//         alert(this.type)
+//     }
+// }
+
+
+// function sound(asd, aasss) {
+//     alert(asd)
+// }
+
+// let aaaa = () => {}
+
+// let him = Person;
+// him.say = sound("anh ny")
+// alert(him.run)
+
