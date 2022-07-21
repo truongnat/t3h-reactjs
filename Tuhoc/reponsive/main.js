26e6dd07de2cd013741b2b47288dcb3cc10497f5
truongdq.dev <truongdq.dev@gmail.com>
2022-07-21 13:43:58 +0700

    refactor sidebar

--- Tuhoc/reponsive/main.js
+++ Tuhoc/reponsive/main.js
@@ -1,32 +1,32 @@
-const $ = document.querySelector.bind(document)
-const $$ = document.querySelectorAll.bind(document)
-const $$$ = $$('.service-content-right-img');
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+const $$$ = $$(".service-content-right-img");
 $$$.forEach((hover, index) => {
-    hover.onclick = function(){
-        $('.service-content-right-img.active').classList.remove('active');
-        this.classList.add('active');
-    }
+  hover.onclick = function () {
+    $(".service-content-right-img.active").classList.remove("active");
+    this.classList.add("active");
+  };
 });
 
-
-// 
+//
 
 var menu = document.getElementById("header-top");
-var menuCenter = document.getElementById("menu-center");
-var listItem = document.getElementById("list-item");
-var transLate = listItem.translateX;
-
+var sidebar = document.querySelector(".sidebar");
 
 var dots = document.getElementById("dots");
-
-
-dots.onclick = function()
-{
-    var closeMenu = menucCenter.translateX===transLate;
-    if (closeMenu){
-        menucCenter.style.translateX =-320 + "px";
-    }
-    else{
-    menucCenter.style.translateX = 320 + "px";
+var toggle = false;
+dots.onclick = () => {
+  toggle = true;
+  toggleMenu();
+};
+function toggleMenu() {
+  let translateX = toggle ? "translateX(0)" : "translateX(100%)";
+  sidebar.style.transform = translateX;
 }
-}
+
+window.addEventListener("click", (e) => {
+  if (!dots.contains(e.target)) {
+    toggle = false;
+    toggleMenu();
+  }
+});
